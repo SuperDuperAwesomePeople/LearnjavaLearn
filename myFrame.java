a156527d3723087aefa040a1f9013beb641b40a3
JPiazza23 <jackpiazza890@gmail.com>
2015-07-12 10:52:23 -0400

    attributation of ponies

--- myFrame.java
+++ myFrame.java
@@ -7,6 +7,10 @@ import javax.swing.JPanel;
 
 public class MyFrame extends JFrame {
   public static void main ( String [] args ) {
-  
+	  MyFrame grid = new MyFrame();
+      grid.setSize( 300, 300 );
+      grid.setResizable( false );
+      grid.setLocationRelativeTo( null );
+      grid.setLayout( new GridLayout(3,3) );
   }
 }
