b259d9de453decf81b79c2837a2d12ec1b88b917
Taran <tns12@scasd.org>
2015-07-12 11:30:15 -0400

    SUPER DUPER AWESOME

--- myFrame.java
+++ myFrame.java
@@ -7,35 +7,30 @@ import javax.swing.JPanel;
 import java.awt.Color;
 
 public class MyFrame extends JFrame {
-  public static void main ( String [] args ) {
-	  MyFrame grid = new MyFrame();
-	  grid.setSize(300,300);
-      grid.setResizable( false );
-      grid.setLocationRelativeTo( null );
-      grid.setLayout( new GridLayout(5,5) );
+	  public makeGrid() {
+		  MyFrame grid = new MyFrame();
+		  grid.setSize(900,900);
+		  grid.setResizable( false );
+		  grid.setLocationRelativeTo( null );
+		  grid.setLayout( new GridLayout(3,3) );
       
-      Container container = grid.getContentPane();
-      ArrayList < JPanel > components = new ArrayList < JPanel >();
-      JPanel temp = null;
+		  Container container = grid.getContentPane();
+		  ArrayList < JPanel > components = new ArrayList < JPanel >();
+		  JPanel temp = null;
 
-      // Populating Arraylist object.
-      for ( int i = 0; i < 25; i++ )
-      {
-          temp = new JPanel();
-          temp.setSize( 60, 60 );
-          components.add( temp );
-          container.add(temp);
-      }
-
-      grid.setVisible( true );
-      components.get( 0 ).setBackground( Color.BLACK );
-      components.get( 1 ).setBackground( Color.CYAN );
-      components.get( 2 ).setBackground( Color.DARK_GRAY );
-      components.get( 3 ).setBackground( Color.GRAY );
-      components.get( 4 ).setBackground( Color.LIGHT_GRAY );
-      components.get( 5 ).setBackground( Color.BLUE );
-      components.get( 6 ).setBackground( Color.MAGENTA );
-      components.get( 7 ).setBackground( Color.ORANGE );
-      components.get( 8 ).setBackground( Color.GREEN );
-  }
+		  // Populating Arraylist object.
+		  for ( int i = 0; i < 9; i++ )
+		  {
+			  temp = new JPanel();
+			  temp.setSize( 300, 300 );
+			  components.add( temp );
+			  container.add(temp);
+		  }
+		  
+		  return container;
+		  return components;
+		  
+		  grid.setVisible( true );
+		  components.get( 4 ).setBackground( Color.GREEN );
+	  }
 }
