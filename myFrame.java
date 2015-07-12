5ce223e0a7a5cc23c6e80e213922e25c21b9c379
Taran <tns12@scasd.org>
2015-07-12 10:54:52 -0400

    More MAGICAL PONIEZZZZZZ

--- myFrame.java
+++ myFrame.java
@@ -11,6 +11,26 @@ public class MyFrame extends JFrame {
       grid.setSize( 300, 300 );
       grid.setResizable( false );
       grid.setLocationRelativeTo( null );
-      grid.setLayout( new GridLayout(3,3) );
+      grid.setLayout( new GridLayout(5,5) );
+      
+      Container container = myframe.getContentPane();
+      ArrayList < JPanel > components = new ArrayList < JPanel >();
+      JPanel temp = null;
+
+      // Populating Arraylist object.
+      for ( int i = 0; i < 9; i++ )
+      {
+          temp = new JPanel();
+          temp.setSize( 60,60 );
+          components.add( temp );
+          container.add(temp);
+      }
+
+      myframe.pack();
+      myframe.setVisible( true );
+
+      // Accessing components via index.
+      components.get( 5 ).setBackground( Color.BLUE );
+      components.get( 8 ).setBackground( Color.GREEN );
   }
 }
