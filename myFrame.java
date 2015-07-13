8c557e5e24da2b9d1758631149c69a7a8c859392
JPiazza23 <jackpiazza890@gmail.com>
2015-07-13 14:24:43 -0400

    Update myFrame.java

--- myFrame.java
+++ myFrame.java
@@ -5,44 +5,34 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Window;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
 
 public class MyFrame extends JFrame {
-	  MyFrame grid = new MyFrame();
-	  public ArrayList<JPanel> makeGridComponents() {
-		  grid.setSize(900,900);
-		  grid.setResizable( false );
-		  grid.setLocationRelativeTo( null );
-		  grid.setLayout( new GridLayout(3,3) );
-      
-		  Container container = grid.getContentPane();
-		  ArrayList < JPanel > components = new ArrayList < JPanel >();
-		  JPanel temp = null;
+	
+	  public ArrayList<JPanel> makeGridComponents(ArrayList<JPanel> components) {
 
 		  for ( int i = 0; i < 9; i++ )
 		  {
-			  temp = new JPanel();
+			  JPanel temp = new JPanel();
 			  temp.setSize( 300, 300 );
 			  components.add( temp );
-			  container.add(temp);
 		  }
 		  return components;
 	  }
+	  
 
-	  public Container makeGridContainer() {
-		  grid.setSize(900,900);
-		  grid.setResizable( false );
-		  grid.setLocationRelativeTo( null );
-		  grid.setLayout( new GridLayout(3,3) );
-    
-		  Container container = grid.getContentPane();
-		  ArrayList < JPanel > components = new ArrayList < JPanel >();
+	  public Container makeGridContainer(Container container, MyFrame grid) {
 		  JPanel temp = null;
-
+		  container = grid.getContentPane();
 		  for ( int i = 0; i < 9; i++ )
 		  {
 			  temp = new JPanel();
 			  temp.setSize( 300, 300 );
-			  components.add( temp );
 			  container.add(temp);
 		  }
 		  
