1d1da03457444122fd1739c2d887540b3dbf98aa
JPiazza23 <jackpiazza890@gmail.com>
2015-07-13 14:42:31 -0400

    Update myFrame.java

--- myFrame.java
+++ myFrame.java
@@ -5,34 +5,25 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.Window;
-import java.util.ArrayList;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Color;
 
 public class MyFrame extends JFrame {
 	
-	  public ArrayList<JPanel> makeGridComponents(ArrayList<JPanel> components) {
+	  public ArrayList<JPanel> makeGridComponents(ArrayList<JPanel> components, JPanel temp) {
 
+		  temp.setSize( 300, 300 );
 		  for ( int i = 0; i < 9; i++ )
 		  {
-			  JPanel temp = new JPanel();
-			  temp.setSize( 300, 300 );
-			  components.add( temp );
+			  components.add(temp);
 		  }
 		  return components;
 	  }
 	  
 
-	  public Container makeGridContainer(Container container, MyFrame grid) {
-		  JPanel temp = null;
-		  container = grid.getContentPane();
+	  public Container makeGridContainer(Container container, JPanel temp) {
+		  
+		  temp.setSize( 300, 300 );;
 		  for ( int i = 0; i < 9; i++ )
 		  {
-			  temp = new JPanel();
-			  temp.setSize( 300, 300 );
 			  container.add(temp);
 		  }
 		  
